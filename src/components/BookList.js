1b161c4b5d45b0e64d13fd0894f7452b13bf6e0d
Rash <rashidasidik914@gmail.com>
2021-06-21 09:09:11 -0700

    deleted space

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -8,8 +8,8 @@ const BookList =()=> {
             
             {book.map((b) => (
                 <div key = { b.id} style= {{borderLeft:"10px solid royalblue",width:"30%", margin:10,boxShadow:"4px 5px #000"}}>
-                    <h3>{b. name}</h3>
-                    <h3>{b. author}</h3>
+                    <h3>{b.name}</h3>
+                    <h3>{b.author}</h3>
                     <button onClick={()=>deleteBook(b.id)} >X</button>
                      </div>
 
